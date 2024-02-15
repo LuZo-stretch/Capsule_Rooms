25a575eb98f5d7456936e290dd7b3bd0196dd723
joana saraiva <joanafilipa.saraiva@gmail.com>
2024-02-15 22:54:05 +0000

    create capsule and see all bookings

--- app/controllers/capsules_controller.rb
+++ app/controllers/capsules_controller.rb
@@ -1,2 +1,20 @@
 class CapsulesController < ApplicationController
+  def new
+    @capsule = Capsule.new
+  end
+
+  def create
+    @capsule = Capsule.new(capsule_params)
+    if @capsule.save
+      redirect_to root_path, notice: "Capsule was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def capsule_params
+    params.require(:capsule).permit(:name, :address, :description, :price, photos: [])
+  end
 end
