53dc4944d32990e561fa7183b114ef393dc14cc7
Lucia Galarza <lzgalarza@gmail.com>
2024-02-15 12:00:02 +0000

    Added the create action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,10 @@
 class BookingsController < ApplicationController
   def create
+    @booking = Booking.new(booking_params)
+    @capsule = Capsule.find(params[:capsule_id])
+    # @booking.capsule_id = @capsule.id
+    @booking.capsule = @capsule
+    @booking.save
+    redirect_to
   end
 end
