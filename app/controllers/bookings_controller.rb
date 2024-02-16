69f1c8fe6cb884795b419df3f52dbcec588d2cc7
Lucia Galarza <lzgalarza@gmail.com>
2024-02-16 20:09:39 +0000

    bookings create added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,21 @@
 class BookingsController < ApplicationController
+
   def create
     @booking = Booking.new(booking_params)
     @capsule = Capsule.find(params[:capsule_id])
-    # @booking.capsule_id = @capsule.id
-    @booking.capsule = @capsule
-    @booking.save
-    redirect_to
+    @booking.capsule_id = @capsule.id
+    @booking.user_id = current_user.id
+    # @booking.capsule = @capsule
+    if @booking.save
+      redirect_to capsule_path(@capsule)
+    else
+      redirect_to capsule_path(@capsule), status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
