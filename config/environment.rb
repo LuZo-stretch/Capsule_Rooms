059caa583cf1f522b45e534235941db9b4d516db
Anahi Herrera <anahiheru@gmail.com>
2024-02-23 20:27:09 +0100

    added env file, tried to upload images from the seeds

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,3 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-CLOUDINARY_URL= 'cloudinary://534432992133227:1IJ18vg-3161p-KtvTG0fPORVf8@digryowvz'
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "open-uri"
 User.destroy_all
 user = User.create(email: "anahiheru@gmail.com", password: "111111")
-Capsule.create!(name: "NapS Nook Capsule", description: "A cozy space for quick relaxation.", price: 15, user_id: user.id)
+Capsule.create!(name: "Nap Nook Capsule", description: "A cozy space for quick relaxation.", price: 15, user_id: user.id)
 Capsule.create!(name: "Futuristic Escape Capsule", description: "Step into different time periods for a unique experience.", price: 25, user_id: user.id)
 Capsule.create!(name: "Zen Oasis Capsule", description: "Tranquil space for meditation and relaxation.", price: 20, user_id: user.id)
 Capsule.create!(name: "Artistic Retreat Capsule", description: "Unleash creativity in a mini art studio.", price: 30, user_id: user.id)
 Capsule.create!(name: "Ocean Serenity Capsule", description: "Find peace and tranquility by the virtual ocean.", price: 18, user_id: user.id)
 Capsule.create!(name: "Sky High Capsule", description: "Experience the sensation of floating among the clouds.", price: 22, user_id: user.id)
+
+# file1 = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+# capsule1 = Capsule.new(name: "Nap Nook Capsule", description: "A cozy space for quick relaxation.", price: 15, user_id: user.id)
+# capsule1.photo.attach(io: file1, filename: "nes.png", content_type: "image/png")
+# capsule1.save
