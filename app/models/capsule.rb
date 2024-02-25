3fddc406e523b2510659efddd808e2aef5988ed2
Anahi Herrera <anahiheru@gmail.com>
2024-02-25 21:29:53 +0100

    deleted unnecessary migrations

--- app/models/capsule.rb
+++ app/models/capsule.rb
@@ -2,4 +2,8 @@ class Capsule < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
