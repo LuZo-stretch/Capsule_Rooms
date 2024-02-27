33d10ba4eaac2516e3eea7205408dc7b103f7fac
Anahi Herrera <anahiheru@gmail.com>
2024-02-27 13:57:31 +0100

    Worked on the cards to make them clickable, removed booking button for owners, added images in the console we might use later, enhanced the index page, changed logo

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,16 @@ class Booking < ApplicationRecord
   belongs_to :capsule
   validates :start_date, presence: true
   validates :end_date, presence: true
+  # before_save :owner_cannot_book_own_capsule
 
   STATUSES = ["pending", "approved", "declined"]
   # enum status: %w[pending approved declined]
+
+  private
+
+  # def owner_cannot_book_own_capsule
+  #   if current_user.id == self.user_id
+  #     errors.add(:owner_error, "Owners cannot book their own capsules")
+  #   end
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@
 User.destroy_all
 user = User.create(email: "anahiheru@gmail.com", password: "111111")
 Capsule.create!(name: "Nap Nook Capsule", description: "A cozy space for quick relaxation.", price: 15, address: '123 Main Street, YourCity, YC', user_id: user.id)
-Capsule.create!(name: "Futuristic Escape Capsule", description: "Step into different time periods for a unique experience.", price: 25, address: "123 Main St, Springfield, IL 62701", user_id: user.id)
+Capsule.create!(name: "Futuristic Escape Capsule", description: "Step for a unique experience.", price: 25, address: "123 Main St, Springfield, IL 62701", user_id: user.id)
 Capsule.create!(name: "Zen Oasis Capsule", description: "Tranquil space for meditation and relaxation.", price: 20, address: "456 Elm St, Rivertown, NY 10001", user_id: user.id)
 Capsule.create!(name: "Artistic Retreat Capsule", description: "Unleash creativity in a mini art studio.", price: 30, address: "789 Oak St, Lakeside, CA 92040", user_id: user.id)
 Capsule.create!(name: "Ocean Serenity Capsule", description: "Find peace and tranquility by the virtual ocean.", price: 18, address: "321 Pine St, Mountainview, CO 80302", user_id: user.id)
