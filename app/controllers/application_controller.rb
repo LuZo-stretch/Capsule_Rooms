18c253a016d520d158d7ef2c1992c6d2c8640eea
joana saraiva <joanafilipa.saraiva@gmail.com>
2024-02-27 18:32:18 +0000

    add map and put link new_user to capsules index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      capsules_path
+    else
+      new_user_session_path
+    end
+  end
 end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,32 +1,64 @@
 import { Controller } from "@hotwired/stimulus"
+import mapboxgl from 'mapbox-gl'
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 // Connects to data-controller="map"
 export default class extends Controller {
   static values = { apiKey: String, markers: Array }
 
   connect() {
+    console.log("bjhbdj")
     mapboxgl.accessToken = this.apiKeyValue;
 
+    this.element.innerHTML = '';
+
     this.map = new mapboxgl.Map({
-      container: this.element,
-      style: 'mapbox://styles/mapbox/streets-v9',
+    container: this.element,
+      style: 'mapbox://styles/mapbox/streets-v10'
     });
 
     this.#addMarkersToMap();
     this.#fitMapToMarkers();
+    // this.#initializeSearch();
+
+    // this.map.addControl(
+    //   new MapboxGeocoder({
+    //     accessToken: mapboxgl.accessToken,
+    //     mapboxgl: mapboxgl
+    //   })
+    // )
   }
 
+
   #addMarkersToMap() {
+    if (this.markersValue.length > 0) {
     const marker = this.markersValue[0];
     new mapboxgl.Marker()
       .setLngLat([marker.lng, marker.lat])
       .addTo(this.map);
+    }
   }
+
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds();
     const marker = this.markersValue[0];
-    bounds.extend([marker.lng, marker.lat]);
+    bounds.extend([marker.lng, marker.lat])
     this.map.fitBounds(bounds, { padding: 50 });
   }
+
+  toggleMap() {
+    const mapContainer = this.element;
+    mapContainer.style.display = mapContainer.style.display === 'none' ? 'block' : 'none';
+  }
 }
+
+//   #initializeSearch() {
+//     const geocoder = new MapboxGeocoder({
+//       accessToken: mapboxgl.accessToken,
+//       mapboxgl: mapboxgl,
+//       marker: false,
+//     });
+
+//     document.getElementById('map-search').appendChild(geocoder.onAdd(this.map));
+//   }
